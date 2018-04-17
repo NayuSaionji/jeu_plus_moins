c0690a845d4954d0a7df20fa7884552d66860e75
Julien Berrebi <juju.truc@gmail.com>
2018-04-17 21:00:40 +0200

    passage à jquery

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 var app = {
 
   // Selections des differents éléments du HTML
-  divWrapper: document.querySelector('#wrapper'),
-  divInfos: document.querySelector('#infos'),
-  btnInfos: document.querySelector('#btninfo'),
-  divReponse: document.querySelector('#reponse'),
-  divIndication: document.querySelector('#indication'),
-  btnReset: document.querySelector('#reset'),
-  divAskUser: document.querySelector('#askUser'),
-  form: document.querySelector('form'),
-  input: document.querySelector('input'),
-  colorGrey: document.querySelector('#grey'),
-  colorBlue: document.querySelector('#blue'),
-  colorPink: document.querySelector('#pink'),
-  divTentatives: document.querySelector('#tentatives span'),
-  divTimer: document.querySelector('#timer span'),
+  divWrapper: $('#wrapper'),
+  divInfos: $('#infos'),
+  btnInfos: $('#btninfo'),
+  divReponse: $('#reponse'),
+  divIndication: $('#indication'),
+  btnReset: $('#reset'),
+  divAskUser: $('#askUser'),
+  form: $('form'),
+  input: $('input'),
+  colorGrey: $('#grey'),
+  colorBlue: $('#blue'),
+  colorPink: $('#pink'),
+  divTentatives: $('#tentatives span'),
+  divTimer: $('#timer span'),
 
   // Valeurs par défaut des clés
   time: 0,
@@ -31,22 +31,22 @@ var app = {
     app.createAlea();
 
     // Ecoute de la validation du formulaire -> Touche Entrer dans le input ou click sur OK
-    app.form.addEventListener('submit', app.askUser);
+    app.form.on('submit', app.askUser);
 
     // Ecoute du survole et quand le curseur repart du bouton Info
-    app.btnInfos.addEventListener('mouseover', app.informations);
-    app.btnInfos.addEventListener('mouseleave', app.removeInformations);
+    app.btnInfos.on('mouseover', app.informations);
+    app.btnInfos.on('mouseleave', app.removeInformations);
 
     // Ecoute du click sur les boutons de couleur
-    app.colorGrey.addEventListener('click', function () {
+    app.colorGrey.on('click', function () {
       app.changeColor(app.colorGrey.id);
-    })
-    app.colorBlue.addEventListener('click', function () {
+    });
+    app.colorBlue.on('click', function () {
       app.changeColor(app.colorBlue.id);
-    })
-    app.colorPink.addEventListener('click', function () {
+    });
+    app.colorPink.on('click', function () {
       app.changeColor(app.colorPink.id);
-    })
+    });
 
     // Lancement du timer toutes les 1sec
     app.timer = setInterval(app.updateTime, 1000);
@@ -66,26 +66,26 @@ var app = {
     // Desactivation du comportement par défaut du formulaire, soit le refresh de la page
     evt.preventDefault();
 
-    reponse = app.input.value; // On récupère le nombre
-    app.input.value = ''; // On vide l'input
+    var reponse = app.input.val(); // On récupère le nombre
+    app.input.val(''); // On vide l'input
 
     // SI reponse est "null" OU vide OU pas un nombre OU supérieur à 1000 OU inférieur à 1
     if (reponse === null || reponse === '' || isNaN( Number(reponse) ) || Number(reponse) > 1000 || Number(reponse) < 1) {
-      app.divReponse.textContent = 'Incorrect ! Il faut saisir un nombre entre 1 et 1000...';
+      app.divReponse.text('Incorrect ! Il faut saisir un nombre entre 1 et 1000...');
     }
     // SINON S'IL reste des tentatives et que la partie n'est pas gagnée
     else if (app.tentatives > 0 && app.win !== true){
       app.userNb = Number(reponse);
-      app.divReponse.textContent = 'Votre chiffre : '+reponse;
+      app.divReponse.text('Votre chiffre : '+reponse);
       app.play(); // On exécute le traitement du nombre de l'utilisateur
     }
     // SINON S'IL ne reste plus de tentatives
     else if (app.tentatives === 0) {
-      app.divReponse.textContent = 'Vous n\'avez plus de tentatives.'
+      app.divReponse.text('Vous n\'avez plus de tentatives.');
     }
     // SINON la partie est déja gagnée
     else {
-      app.divReponse.textContent = 'Vous avez déjà trouver la réponse.'
+      app.divReponse.text('Vous avez déjà trouver la réponse.');
     }
   },
 
@@ -95,26 +95,26 @@ var app = {
     // SI le nombre a trouver est strictement égal au nombre de l'utilisateur ET que la partie n'est pas encore gagnée
     if (app.aleaNb === app.userNb && app.win !== true) {
       app.win = true;
-      app.divIndication.textContent ='Bravo ! Vous avez trouvé le bon nombre.';
+      app.divIndication.text('Bravo ! Vous avez trouvé le bon nombre.');
       clearInterval(app.timer); // On arrête le chrono
       app.rejouer(); // Apparition du bouton Rejouer
     }
     // SINON SI le nombre a trouver est supérieur au nombre de l'utilisateur ET que la partie n'est pas encore gagnée
     else if (app.aleaNb > app.userNb && app.win !== true) {
       app.tentatives--;
-      app.divTentatives.textContent = app.tentatives;
-      app.divIndication.textContent = 'Votre nombre est trop petit !';
+      app.divTentatives.text(app.tentatives);
+      app.divIndication.text('Votre nombre est trop petit !');
     }
     // SINON SI le nombre a trouver est inférieur au nombre de l'utilisateur ET que la partie n'est pas encore gagnée
     else if (app.aleaNb < app.userNb && app.win !== true) {
       app.tentatives--;
-      app.divTentatives.textContent = app.tentatives;
-      app.divIndication.textContent = 'Votre nombre est trop grand !';
+      app.divTentatives.text(app.tentatives);
+      app.divIndication.text('Votre nombre est trop grand !');
     }
 
     // SI l'utilisateur n'a plus de tentatives
     if (app.tentatives === 0) {
-      app.divIndication.textContent = 'Perdu ! Il fallait trouver : ' + app.aleaNb;
+      app.divIndication.text('Perdu ! Il fallait trouver : ' + app.aleaNb);
       clearInterval(app.timer); // On arrête le chrono
       app.rejouer(); // Apparition du bouton Rejouer
     }
@@ -122,47 +122,47 @@ var app = {
 
   // Méthode permettant de faire apparaître le bouton Rejouer
   rejouer: function () {
-    app.btnReset.style.display = 'initial';
-    app.btnReset.addEventListener('click', app.clean);
+    app.btnReset.css('display', 'initial');
+    app.btnReset.on('click', app.clean);
   },
 
   // Méthode permettant de remettre le jeu aux valeurs d'origine
   clean: function () {
     app.createAlea(); // Génération d'un nouveau nombre aléatoire
-    app.divReponse.textContent = '';
-    app.divIndication.textContent = '';
+    app.divReponse.text('');
+    app.divIndication.text('');
     app.win = false;
     app.tentatives = 10;
-    app.divTentatives.textContent = app.tentatives;
-    app.btnReset.style.display = 'none'; // On enlève le bouton Rejouer
+    app.divTentatives.text(app.tentatives);
+    app.btnReset.css('display', 'none'); // On enlève le bouton Rejouer
     app.resetTime(); // On remet le chrono a 0 et on le relance
   },
 
   // Méthode permettant de changer le background-color du jeu au click sur les carrés
   changeColor: function (idColor) {
     switch (idColor) {
-      case 'grey':
-        app.divWrapper.style.backgroundColor = '#eee';
-        break;
-      case 'blue':
-        app.divWrapper.style.backgroundColor = 'lightblue';
-        break;
-      case 'pink':
-        app.divWrapper.style.backgroundColor = 'pink';
-        break;
-      default:
+    case 'grey':
+      app.divWrapper.css('background-color','#eee');
+      break;
+    case 'blue':
+      app.divWrapper.css('background-color','lightblue');
+      break;
+    case 'pink':
+      app.divWrapper.css('background-color','pink');
+      break;
+    default:
     }
   },
 
   // Méthode pour mettre à jour le chrono
   updateTime: function () {
     app.time++;
-    app.divTimer.textContent = app.time;
+    app.divTimer.text(app.time);
 
     // SI le chrono arrive à 4 chiffres, on réduit la font-size pour que ca ne dépasse pas du cadre
     if (app.time >= 1000) {
-      app.divTimer.style.fontSize = '3rem';
-      app.divTimer.style.lineHeight = '1.5';
+      app.divTimer.css('font-size', '3rem');
+      app.divTimer.css('line-height', '1.5');
     }
   },
 
@@ -172,22 +172,22 @@ var app = {
     app.time = 0;
     clearInterval(app.timer);
     app.timer = setInterval(app.updateTime, 1000);
-    app.divTimer.textContent = app.time;
+    app.divTimer.text(app.time);
   },
 
   // Méthode pour rendre visible les informations sur le jeu et mettre la div au niveau du curseur
   informations: function(evt) {
 
-    app.divInfos.style.visibility = 'visible';
-    app.divInfos.style.left = evt.clientX+20+'px';
-    app.divInfos.style.top = evt.clientY-50+'px';
+    app.divInfos.css('visibility', 'visible');
+    app.divInfos.css('left', evt.clientX+20+'px');
+    app.divInfos.css('top', evt.clientY-50+'px');
   },
 
   // Méthode pour cacher les informations quand le curseur part du bouton Info
   removeInformations: function() {
 
-    app.divInfos.style.visibility = 'hidden';
+    app.divInfos.css('visibility', 'hidden');
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', app.init);
+$(app.init);
